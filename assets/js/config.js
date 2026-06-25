12142286456f2f4ef2bf6c3c9da172e901eefc1e
nqthtmlcssjs <nqt.tt.md.hn@gmail.com>
2026-06-25 08:57:22 +0700

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,6 +0,0 @@
-window.CN10_CONFIG = {
-  APP_VERSION: '1.0.0',
-  APP_NAME: 'CN10 Fee Control Center Enterprise',
-  SUPABASE_URL: 'https://tstxzuuyrpzwhuggzqvd.supabase.co',
-  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_M012zDkENo6yKEL_Y2gblQ_F7Jytn0Z'
-};
